df08c728eb1ca322aa026bb64db559df04185418
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 09:17:10 +0000

    Deploy jolylai/notebook to github.com/jolylai/notebook.git:gh-pages

--- assets/js/59.21fefa38.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[59],{211:function(n,w,o){}}]);
---
+++ assets/js/60.bae9c18b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[60],{211:function(n,w,o){}}]);
