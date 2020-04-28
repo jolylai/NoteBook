b4d3851a95a139d5bf938c837b4482c6ac6be39b
jolylai <jolylai163@163.com>
2020-04-28 10:35:50 +0800

    refactor: .umi

--- packages/advance/src/.umi/core/plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from '/Users/laiguolin/Workspace/notebook/notebook/node_modules/@umijs/runtime/dist/index.js';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
